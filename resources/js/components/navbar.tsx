37d22d82bb9c80431c8c87df90c41fe6b6a126c5
GayerLeonet <gabrielleonett@gmail.com>
2025-03-21 20:32:58 -0400

    Navbar 90%

--- resources/js/components/navbar.tsx
+++ resources/js/components/navbar.tsx
@@ -1,104 +1,131 @@
-import * as React from 'react';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import MenuIcon from '@mui/icons-material/Menu';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import FacebookIcon from '@mui/icons-material/Facebook';
 import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import LogoTexto from './ui/logoTexto';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
 import LogoSimple from './ui/logoSimple';
+import LogoTexto from './ui/logoTexto';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
-const pages = ['Products', 'Pricing', 'Blog'];
+type PropsAppBar = {
+    pages: string[];
+    backgroundColor?: boolean;
+};
 
-function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+function ResponsiveAppBar({ pages, backgroundColor }: PropsAppBar) {
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
-  return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          {/* LogoTexto: Visible en pantallas grandes (md y arriba) */}
-          <Box sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center', height: '100px' }}>
-            <LogoTexto Primary='bg-white' Secundary='bg-secundary' />
-          </Box>
+    // Función para redirigir a una URL
+    const handleRedirect = (url: string) => {
+        window.location.href = url;
+    };
 
-          {/* LogoSimple: Visible en pantallas pequeñas y medianas (xs, sm) */}
-          <Box sx={{ display: { xs: 'flex', md: 'none' }, alignItems: 'center', height: '100px' }}>
-            <LogoSimple Primary='bg-white' Secundary='bg-secundary' />
-          </Box>
+    return (
+        <AppBar position="fixed" sx={{ backgroundColor: backgroundColor ? 'bg-primary' : 'transparent' }}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    {/* LogoTexto: Visible en pantallas grandes (md y arriba) */}
+                    <Box sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center', height: '100px' }}>
+                        <LogoTexto Primary="bg-white" Secundary="bg-secundary" />
+                    </Box>
 
-          {/* Menú para pantallas pequeñas */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, fontFamily: 'Poppins' }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: 'block', md: 'none' } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography variant='body1'>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+                    {/* LogoSimple: Visible en pantallas pequeñas y medianas (xs, sm) */}
+                    <Box sx={{ display: { xs: 'flex', md: 'none' }, alignItems: 'center', height: '100px' }}>
+                        <LogoSimple Primary="bg-white" Secundary="bg-secundary" />
+                    </Box>
 
-          {/* Menú para pantallas grandes */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+                    {/* Menú para pantallas pequeñas */}
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, fontFamily: 'Poppins' }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{ display: { xs: 'block', md: 'none' } }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem
+                                    key={page}
+                                    onClick={() => {
+                                        handleRedirect(`/${page.toLowerCase()}`); // Redirige a la URL
+                                        handleCloseNavMenu(); // Cierra el menú
+                                    }}
+                                    sx={{ textDecoration: 'none', color: 'inherit' }}
+                                >
+                                    <Typography variant="body1">
+                                        {page}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
 
-          {/* Icono de Facebook */}
-          <Box sx={{ flexGrow: 0 }}>
-            <IconButton onClick={() => { window.location.href = 'https://www.facebook.com'; }}>
-              <FacebookIcon />
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+                    {/* Menú para pantallas grandes */}
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={() => handleRedirect(`/${page.toLowerCase()}`)} // Redirige a la URL
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    {/* Icono de Facebook */}
+                    <Box sx={{ flexGrow: 0 }}>
+                        <IconButton
+                            onClick={() => handleRedirect('https://www.facebook.com')} // Redirige a Facebook
+                            sx={{ color: 'white' }} // Aplica color blanco al IconButton
+                        >
+                            <FacebookIcon sx={{ color: 'inherit' }} /> {/* Hereda el color del IconButton */}
+                        </IconButton>
+                        <IconButton 
+                        sx={{ color: 'white' }} 
+                        onClick={() => handleRedirect('/login')}
+                        >
+                            <AccountCircleOutlinedIcon/> {/* Hereda el color del IconButton */}
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
 
 export default ResponsiveAppBar;
--- resources/js/pages/index.tsx
+++ resources/js/pages/index.tsx
@@ -1,17 +1,29 @@
-import { Head } from "@inertiajs/react";
-import ResponsiveAppBar from "@/components/navbar";
-import { useState } from "react";
+import ResponsiveAppBar from '@/components/navbar';
+import { Head } from '@inertiajs/react';
+import Typography from '@mui/material/Typography';
 
 export default function Index() {
-    const [darkmode, setDarkMode] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches);
-
     return (
-        <div>
+        <>
             <Head title="Inicio" />
-            <ResponsiveAppBar/>
-            <br />
-            <br />
-            <button onClick={()=>{setDarkMode(!darkmode)}}>DarkMode</button>
-        </div>
+            <ResponsiveAppBar pages={['Universidad','Academico','Servicios','Tramites']} />
+            <section className="relative flex h-[48rem] items-center justify-end">
+                {/* Fondo con imagen y gradiente */}
+                <div className="absolute inset-0 bg-cover bg-center h-<100%>" style={{ backgroundImage: "linear-gradient(to bottom, hsla(0, 0.00%, 19.20%, 0.60), rgba(0, 0, 0, 0.81)), url('/storage/estudiantes1.jpg')", }} aria-hidden="true"></div>
+                <div className="relative max-w-2xl rounded-lg p-8 text-center text-white">
+                    <Typography component="h1" variant="h3" gutterBottom>
+                        «Enséñese lo que se entienda, enséñese lo que sea útil, enséñese a todos; y eso es todo.»
+                    </Typography>
+                    <Typography component="h2" variant="h4">
+                        Cecilio Acosta
+                    </Typography>
+                </div>
+            </section>
+            <Typography variant='h2' component={'h2'}>
+                Noticias
+            </Typography>
+            <hr />
+            
+        </>
     );
 }
