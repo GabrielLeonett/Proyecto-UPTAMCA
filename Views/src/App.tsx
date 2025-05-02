10cb85dbd586ec5bc40cca680fb583013fb6bfc1
GayerLeonet <gabrielleonett@gmail.com>
2025-05-02 08:41:49 -0400

    Actualizacion de carpetas

---
+++ Views/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-700">Welcome to the App</h1>
+    </div>
+  );
+}
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+export const authRouter = Router();
+
+//Rutas de autenticacion
+authRouter.post("/login", )
+authRouter.post("/register", )
+authRouter.post("/logout", )
