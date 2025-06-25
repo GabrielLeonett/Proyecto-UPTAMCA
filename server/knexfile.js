885850085f96fe84490fe4161e98140c046a945f
GayerLeonet <gabrielleonett@gmail.com>
2025-06-25 14:58:37 -0400

    despliegue del backend completado

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,42 +1,39 @@
 import dotenv from "dotenv";
 
-const envFile = `.env.${process.env.NODE_ENV || 'development'}`;
+const envFile = `.env.${process.env.NODE_ENV || "development"}`;
 
 dotenv.config({ path: envFile }); // Cargar el archivo correspondiente
 
 // knexfile.js
 export default {
   development: {
-    client: 'pg',  // o 'pg', 'sqlite3', etc.
+    client: "pg", // o 'pg', 'sqlite3', etc.
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
     },
     migrations: {
-      directory: './database/migrations',
+      directory: "./database/migrations",
     },
     seeds: {
-      directory: './database/seeds',
+      directory: "./database/seeds",
     },
   },
-  deploy: {
-    client: 'pg',  // o 'pg', 'sqlite3', etc.
+  production: {
+    client: "pg", // o 'pg', 'sqlite3', etc.
     connection: {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       port: process.env.DB_PORT,
-      ssl:{ rejectUnauthorized: true }
+      ssl: { rejectUnauthorized: false },
     },
     migrations: {
-      directory: './database/migrations',
+      directory: "./database/migrations",
     },
     seeds: {
-      directory: './database/seeds',
+      directory: "./database/seeds",
     },
   },
 };
