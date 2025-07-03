d9e89b36007af5d6ca01c53627cbcdc3117b6aea
GayerLeonet <gabrielleonett@gmail.com>
2025-07-03 10:50:47 -0400

    Creacion de una ruta para verificar los reportes

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { middlewareAuth } from "../middlewares/auth.js";
 import UserController from '../controllers/UserController.js'
+import fs from 'node:fs'
+import { error } from "node:console";
+import { date } from "zod";
+import { json } from "node:stream/consumers";
 
 const { login, verificarUsers } = UserController;
 
@@ -19,3 +23,10 @@ export const UserRouter = Router();
 UserRouter.post('/login', login)
 
 UserRouter.get('/vefiry', middlewareAuth(null), verificarUsers)
+
+UserRouter.get('/report', middlewareAuth('SuperAdmin'), (req,res)=>{
+    fs.readFile('./report.json', (err, date)=>{
+        res.json(JSON.parse(date));
+        console.log(err)
+    })
+})
